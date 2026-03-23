5e11b3ef022531dedb05f10fa1cae9bc9536b44c
gabreilbadayos <gabreilbads@gmail.com>
2026-03-24 00:21:58 +0800

    Fix database - use SQLite for simpler deployment

--- story_book/settings.py
+++ story_book/settings.py
@@ -53,25 +53,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'story_book.wsgi.application'
 
-# Database - Use PostgreSQL on Render, SQLite locally
-if os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_DB', 'storybook'),
-            'USER': os.environ.get('POSTGRES_USER', 'storybook'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
-            'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-            'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-        }
+# Database - Use SQLite for now (simpler for deployment)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
